e9e9b564fbd05059d8fb36a7517be4025f525559
stile21 <stile21@stile21.net>
2025-08-06 09:12:57 +0200

    Add files via upload

--- drive_utils.py
+++ drive_utils.py
@@ -1,51 +1,59 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 import os
+import io
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+import streamlit as st
 
-def connect_drive():
-    gauth = GoogleAuth()
-    
-    # Configura PyDrive per usare la chiave del service account
-    gauth.settings['client_config_backend'] = 'service'
-    gauth.settings['service_config'] = {
-        "client_service_email": "streamlit-bot@dashboardstile21.iam.gserviceaccount.com",
-        "client_user_email": "",
-        "private_key_file": "service_account.json"
+
+def get_drive_service():
+    creds_dict = st.secrets["google_service_account"]
+    creds = service_account.Credentials.from_service_account_info(creds_dict)
+    return build("drive", "v3", credentials=creds)
+
+
+def get_or_create_drive_folder(service, folder_name):
+    # Verifica se la cartella esiste
+    query = f"name = '{folder_name}' and mimeType = 'application/vnd.google-apps.folder' and trashed = false"
+    results = service.files().list(q=query, spaces="drive", fields="files(id, name)").execute()
+    files = results.get("files", [])
+
+    if files:
+        return files[0]["id"]
+
+    # Altrimenti crea la cartella
+    file_metadata = {
+        "name": folder_name,
+        "mimeType": "application/vnd.google-apps.folder"
     }
+    file = service.files().create(body=file_metadata, fields="id").execute()
+    return file.get("id")
 
-    gauth.ServiceAuth()
-    return GoogleDrive(gauth)
 
-def get_or_create_drive_folder(drive, folder_name):
-    file_list = drive.ListFile({
-        'q': f"title='{folder_name}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
-    }).GetList()
+def upload_file_to_drive(file_path, service, folder_id):
+    file_name = os.path.basename(file_path)
+    file_metadata = {
+        "name": file_name,
+        "parents": [folder_id]
+    }
+    media = MediaFileUpload(file_path, resumable=True)
+    service.files().create(body=file_metadata, media_body=media, fields="id").execute()
 
-    if file_list:
-        return file_list[0]['id']
-    else:
-        folder_metadata = {
-            'title': folder_name,
-            'mimeType': 'application/vnd.google-apps.folder'
-        }
-        folder = drive.CreateFile(folder_metadata)
-        folder.Upload()
-        return folder['id']
 
-def upload_file_to_drive(local_path, drive, folder_id):
-    file_drive = drive.CreateFile({
-        'title': os.path.basename(local_path),
-        'parents': [{'id': folder_id}]
-    })
-    file_drive.SetContentFile(local_path)
-    file_drive.Upload()
+def download_all_from_drive(service, folder_id, local_dir):
+    if not os.path.exists(local_dir):
+        os.makedirs(local_dir)
 
-def download_all_from_drive(drive, folder_id, local_dir):
-    os.makedirs(local_dir, exist_ok=True)
-    file_list = drive.ListFile({
-        'q': f"'{folder_id}' in parents and trashed=false"
-    }).GetList()
+    query = f"'{folder_id}' in parents and trashed = false"
+    results = service.files().list(q=query, fields="files(id, name)").execute()
+    files = results.get("files", [])
 
-    for file in file_list:
-        file_path = os.path.join(local_dir, file['title'])
-        file.GetContentFile(file_path)
+    for file in files:
+        file_id = file["id"]
+        file_name = file["name"]
+        request = service.files().get_media(fileId=file_id)
+        fh = io.FileIO(os.path.join(local_dir, file_name), "wb")
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while not done:
+            _, done = downloader.next_chunk()
